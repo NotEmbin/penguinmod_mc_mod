ae5eaab4e9dbca52cfdd0976b6fdd7f9cfe26126
Embin <113154360+NotEmbin@users.noreply.github.com>
2024-08-26 21:09:23 -0700

    paintings and blocks

--- src/main/java/com/penguinmod/PenguinMod.java
+++ src/main/java/com/penguinmod/PenguinMod.java
@@ -1,15 +1,34 @@
 package com.penguinmod;
 
+import com.mojang.serialization.JsonOps;
+import com.penguinmod.util.ConvertNamespace;
+import com.penguinmod.util.Tags;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.command.EntityDataObject;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.CustomModelDataComponent;
+import net.minecraft.component.type.NbtComponent;
+import net.minecraft.entity.EntityData;
+import net.minecraft.entity.decoration.painting.PaintingEntity;
+import net.minecraft.entity.decoration.painting.PaintingVariant;
+import net.minecraft.entity.decoration.painting.PaintingVariants;
 import net.minecraft.item.*;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.predicate.item.CustomDataPredicate;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryOps;
+import net.minecraft.registry.tag.TagBuilder;
+import net.minecraft.registry.tag.TagEntry;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.text.Text;
+import net.minecraft.text.TextCodecs;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,9 +54,28 @@ public class PenguinMod implements ModInitializer {
 		}
 
 		public static final Block JOE_BLOCK = register(new Block(AbstractBlock.Settings.create()), "joe_block", true);
+		public static final Block DDEDEDODEDIAMANTE_BLOCK = register(new Block(AbstractBlock.Settings.create()), "ddededodediamante_block", true);
+		public static final Block PUZZLINGGGG_BLOCK = register(new Block(AbstractBlock.Settings.create()), "puzzlingggg_block", true);
+		public static final Block JEREMYGAMER13_BLOCK = register(new Block(AbstractBlock.Settings.create()), "jeremygamer13_block", true);
+		public static final Block IANYOURGOD_BLOCK = register(new Block(AbstractBlock.Settings.create()), "ianyourgod_block", true);
+		public static final Block GODSLAYERAKP_BLOCK = register(new Block(AbstractBlock.Settings.create()), "godslayerakp_block", true);
+		public static final Block EVIL_JOE_BLOCK = register(new Block(AbstractBlock.Settings.create()), "evil_joe_block", true);
+		public static final Block EVIL_PUZZLINGGGG_BLOCK = register(new Block(AbstractBlock.Settings.create()), "evil_puzzlingggg_block", true);
+		public static final Block ALPACALII_BLOCK = register(new Block(AbstractBlock.Settings.create()), "alpacalii_block", true);
+		public static final Block SHARKPOOL_BLOCK = register(new Block(AbstractBlock.Settings.create()), "sharkpool_block", true);
+		public static final Block CUBEY_BLOCK = register(new Block(AbstractBlock.Settings.create()), "cubey_block", true);
+		public static final Block JOHN_FORTNITE_BLOCK = register(new Block(AbstractBlock.Settings.create()), "john_fortnite_block", true);
+		public static final Block ELMOBEAR_BLOCK = register(new Block(AbstractBlock.Settings.create()), "elmobear_block", true);
+		public static final Block FRESHPENGUIN112_BLOCK = register(new Block(AbstractBlock.Settings.create()), "freshpenguin112_block", true);
+		public static final Block ANONYGOOSE_BLOCK = register(new Block(AbstractBlock.Settings.create()), "anonygoose_block", true);
+		public static final Block JWKLONG_BLOCK = register(new Block(AbstractBlock.Settings.create()), "jwklong_block", true);
+		public static final Block STEVE0GREATNESS_BLOCK = register(new Block(AbstractBlock.Settings.create()), "steve0greatness_block", true);
+		public static final Block PENGUINBOT_BLOCK = register(new Block(AbstractBlock.Settings.create()), "penguinbot_block", true);
+		public static final Block SHADOWV4ST_BLOCK = register(new Block(AbstractBlock.Settings.create()), "shadowv4st_block", true);
+		public static final Block XENDYEX_BLOCK = register(new Block(AbstractBlock.Settings.create()), "xendyex_block", true);
+		public static final Block TIM_SWEENEY_BLOCK = register(new Block(AbstractBlock.Settings.create()), "tim_sweeney_block", true);
 
-		public static void init() {
-		}
+		public static void init() {}
 	}
 
 	public final class PMItems {
@@ -50,18 +88,44 @@ public class PenguinMod implements ModInitializer {
 			return Registry.register(Registries.ITEM, cn.convertNamespace(path), item);
 		}
 
-		public static void init() {
-		}
+		public static void init() {}
 	}
 
 	public final class PMItemGroups {
+		public static void paintingVariants(
+		) {
+			//RegistryOps<TagKey> registryops = Registr
+		}
+
 		public static final ItemGroup PM_GROUP = FabricItemGroup.builder()
 			.icon(() -> new ItemStack(PMItems.STINKY_ITEM))
-			.displayName(Text.translatable("itemGroup.penguinmod.pm_group"))
+			.displayName(Text.translatable("itemGroup.penguinmod"))
 			.entries((context, entries) -> {
 				entries.add(PMItems.STINKY_ITEM);
-				entries.add(PMBlocks.JOE_BLOCK);
 				entries.add(PMItems.TEST_ITEM);
+				entries.add(PMBlocks.JOE_BLOCK);
+				entries.add(PMBlocks.DDEDEDODEDIAMANTE_BLOCK);
+				entries.add(PMBlocks.PUZZLINGGGG_BLOCK);
+				entries.add(PMBlocks.JEREMYGAMER13_BLOCK);
+				entries.add(PMBlocks.IANYOURGOD_BLOCK);
+				entries.add(PMBlocks.GODSLAYERAKP_BLOCK);
+				entries.add(PMBlocks.EVIL_JOE_BLOCK);
+				entries.add(PMBlocks.EVIL_PUZZLINGGGG_BLOCK);
+				entries.add(PMBlocks.ALPACALII_BLOCK);
+				entries.add(PMBlocks.SHARKPOOL_BLOCK);
+				entries.add(PMBlocks.CUBEY_BLOCK);
+				entries.add(PMBlocks.JOHN_FORTNITE_BLOCK);
+				entries.add(PMBlocks.ELMOBEAR_BLOCK);
+				entries.add(PMBlocks.FRESHPENGUIN112_BLOCK);
+				entries.add(PMBlocks.ANONYGOOSE_BLOCK);
+				entries.add(PMBlocks.JWKLONG_BLOCK);
+				entries.add(PMBlocks.STEVE0GREATNESS_BLOCK);
+				entries.add(PMBlocks.PENGUINBOT_BLOCK);
+				entries.add(PMBlocks.SHADOWV4ST_BLOCK);
+				entries.add(PMBlocks.XENDYEX_BLOCK);
+				entries.add(PMBlocks.TIM_SWEENEY_BLOCK);
+				//entries.add(new ItemStack(Items.PAINTING).set(DataComponentTypes.ENTITY_DATA, NbtComponent.DEFAULT.apply(PaintingVariants.TIDES)));
+
 			})
 			.build();
 
--- src/main/java/com/penguinmod/util/ConvertNamespace.java
+++ src/main/java/com/penguinmod/util/ConvertNamespace.java
@@ -1,12 +1,13 @@
-package com.penguinmod;
+package com.penguinmod.util;
 
+import com.penguinmod.PenguinMod;
 import net.minecraft.util.Identifier;
 
 public class ConvertNamespace {
     public Identifier convertNamespace(String namespace) {
         String[] splitted = namespace.split(":");
         if (splitted.length == 1) {
-            return Identifier.of("penguinmod", namespace);
+            return Identifier.of(PenguinMod.MOD_ID, namespace);
         }
         return Identifier.of(splitted[0],splitted[1]);
     }
